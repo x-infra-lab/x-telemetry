8fc4eb572e542445d72dafb03422569e6414d578
jiangyuan <469391363@qq.com>
2024-08-23 14:09:00 +0000

    update

--- x-telemetry/x-telemetry-agent/src/main/java/io/github/xinfra/lab/telemetry/agent/XTelemetryAgent.java
+++ x-telemetry/x-telemetry-agent/src/main/java/io/github/xinfra/lab/telemetry/agent/XTelemetryAgent.java
@@ -1,12 +1,14 @@
 package io.github.xinfra.lab.telemetry.agent;
 
+import io.github.xinfra.lab.telemetry.config.ConfigManager;
+
 /**
  * x-telemetry java-agent
  */
-public class XTelemetryAgent
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class XTelemetryAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+
+        ConfigManager configManager = new ConfigManager();
+        // configManager.load();
     }
 }
