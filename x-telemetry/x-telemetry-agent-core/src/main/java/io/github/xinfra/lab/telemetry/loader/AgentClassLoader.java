ee409b1d087fa8b1c961161060b4cac72ed3c4f4
joecqupt <joe469391363@gmail.com>
2024-11-27 01:16:54 +0800

    update

---
+++ x-telemetry/x-telemetry-agent-core/src/main/java/io/github/xinfra/lab/telemetry/loader/AgentClassLoader.java
@@ -0,0 +1,9 @@
+package io.github.xinfra.lab.telemetry.loader;
+
+public class AgentClassLoader extends ClassLoader {
+    static {
+        registerAsParallelCapable();
+    }
+
+
+}
--- x-telemetry/x-telemetry-agent-core/src/main/java/io/github/xinfra/lab/telemetry/plugin/AbstractClassEnhancePlugin.java
+++ x-telemetry/x-telemetry-agent-core/src/main/java/io/github/xinfra/lab/telemetry/plugin/AbstractClassEnhancePlugin.java
@@ -1,4 +1,26 @@
 package io.github.xinfra.lab.telemetry.plugin;
 
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+
 public abstract class AbstractClassEnhancePlugin {
+
+    abstract ElementMatcher<TypeDescription> enhanceClass();
+
+    protected ElementMatcher<TypeDescription> witnessClasses() {
+        // todo
+        return null;
+    }
+
+    protected ElementMatcher<MethodDescription> witnessMethods() {
+        // todo
+        return null;
+    }
+
+    public boolean isBootstrapInstrumentation() {
+        // todo
+        return false;
+    }
 }
